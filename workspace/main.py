6b98c4801986a2b9e2e16b2028463b895693cb6e
takahito hirose <taks@takahitonoMacBook-Air.local>
2026-03-05 20:00:26 +0900

    feat: add script to display 'ARK System Online!' in red using colorama

--- workspace/main.py
+++ workspace/main.py
@@ -1,1 +1,17 @@
-print('hello')
+"""
+Main module of the ARK System Online project.
+"""
+
+from colorama import Fore, Style
+
+def print_ark_system_online() -> None:
+    """
+    Prints 'ARK System Online!' in red color using the colorama package.
+
+    Returns:
+        None
+    """
+    print(Fore.RED + 'ARK System Online!' + Style.RESET_ALL)
+
+if __name__ == "__main__":
+    print_ark_system_online()
